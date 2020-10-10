a624efbb9148246b874980aa8c0801046bda56ec
MG <m@sudo.eu>
2020-10-10 21:06:33 +0200

    first version

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,6 +1,45 @@
+import { CommonModule } from "@angular/common";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  NgModule,
+  Output
+} from "@angular/core";
+import { MockBuilder, MockRender, ngMocks } from "ng-mocks";
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// you can check more examples in src/e2e.ts
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+@Component({
+  selector: "app-target",
+  template: `<a (click)="click.emit()">name: {{ name }}</a>`
+})
+class TargetComponent {
+  @Input() public readonly name: string;
+  @Output() public readonly click: EventEmitter<void> = new EventEmitter();
+}
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TargetComponent]
+})
+class TargetModule {}
+
 describe("my sandbox", () => {
-  it("should something", () => {
-    
-    expect(1).toBe(2);
+  beforeEach(() => MockBuilder(TargetComponent, TargetModule));
+
+  it("should do something", () => {
+    const spy = jasmine.createSpy();
+    const fixture = MockRender(TargetComponent, {
+      name: "sandbox",
+      click: spy
+    });
+    expect(fixture.point.nativeElement.innerHTML).toContain("name: sandbox");
+
+    const link = ngMocks.find(fixture.debugElement, "a");
+    expect(spy).not.toHaveBeenCalled();
+    link.triggerEventHandler("click", null);
+    expect(spy).toHaveBeenCalled();
   });
 });
