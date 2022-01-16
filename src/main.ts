f23873b84bc650aa71fc3b2feb287f24c353108b
satanTime <m@sudo.eu>
2022-01-16 20:13:33 +0100

    fix: restoring old jasmine

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import './polyfills';
 
 import 'jasmine-core/lib/jasmine-core/jasmine.js';
 import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
-import 'jasmine-core/lib/jasmine-core/boot0.js';
-import 'jasmine-core/lib/jasmine-core/boot1.js';
+import 'jasmine-core/lib/jasmine-core/boot.js';
 
 import 'zone.js/dist/zone-testing';
 
