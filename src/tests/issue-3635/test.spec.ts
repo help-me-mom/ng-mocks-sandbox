343f3014f766891c2b619c4c24782efe465be227
satanTime <m@sudo.eu>
2022-09-25 12:01:33 +0200

    chore(deps): update dependency ng-mocks to v14.2.3

---
+++ src/tests/issue-3635/test.spec.ts
@@ -0,0 +1,43 @@
+import { CommonModule, NgIf } from '@angular/common';
+import {
+  ApplicationModule,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+import { MockBuilder } from 'ng-mocks';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: ` <a [routerLink]="['link']">Link</a> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MyComponent {
+  constructor(public activatedRoute: ActivatedRoute) {}
+}
+
+// @see https://github.com/help-me-mom/ng-mocks/issues/3635
+// MockBuilder doesn't detect always keep modules such as CommonModule.
+describe('issue-3635', () => {
+  it('does not throw because CommonModule is an import in MyComponent', () => {
+    expect(() =>
+      MockBuilder(MyComponent, ActivatedRoute).build(),
+    ).not.toThrowError(/MockBuilder has found a missing dependency/);
+  });
+
+  it('throws because ApplicationModule is not imported anywhere', () => {
+    expect(() =>
+      MockBuilder(MyComponent, ActivatedRoute)
+        .mock(ApplicationModule)
+        .build(),
+    ).toThrowError(/MockBuilder has found a missing dependency/);
+  });
+
+  it('does not throw because NgIf is a part of CommonModule from MyComponent', () => {
+    expect(() =>
+      MockBuilder(MyComponent, ActivatedRoute).mock(NgIf).build(),
+    ).not.toThrowError(/MockBuilder has found a missing dependency/);
+  });
+});
---
+++ src/tests/issue-3636/test.spec.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { MockBuilder, MockRender } from 'ng-mocks';
+
+@Component({
+  standalone: true,
+  template: ``,
+})
+export class MyComponent {}
+
+// @see https://github.com/help-me-mom/ng-mocks/issues/3636
+// The problem was that a standalone component caused MockRender
+// to create the middleware component as standalone too.
+// The fix ensures that the middleware component is always a normal declaration.
+describe('issue-3636', () => {
+  beforeEach(() => MockBuilder(MyComponent));
+
+  it('detects the standalone component correctly in MockRender', () => {
+    const fixture = MockRender(MyComponent);
+    expect(fixture.point.componentInstance).toBeDefined();
+  });
+});
