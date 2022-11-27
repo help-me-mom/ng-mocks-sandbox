e958bc34338ef5c40a0dfd155c4e311b67a30dc7
satanTime <m@sudo.eu>
2022-11-27 18:46:11 +0100

    chore(deps): update ng-mocks to v14.4.0

---
+++ src/tests/issue-4032/test.spec.ts
@@ -0,0 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+
+@Component({
+  selector: 'my-test-one',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `<a [routerLink]="['test']">Test</a>`,
+})
+class TestOneComponent {}
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `<a [routerLink]="['test']">Test</a>`,
+})
+class TestTwoComponent {}
+
+// @see https://github.com/help-me-mom/ng-mocks/issues/4032
+describe('issue-4032', () => {
+  ngMocks.throwOnConsole();
+
+  describe('w/ selector', () => {
+    beforeEach(() => MockBuilder(TestOneComponent));
+
+    it('should render w/ MockRender', () => {
+      expect(
+        MockRender(TestOneComponent).point.componentInstance,
+      ).toBeDefined();
+    });
+
+    it('should render w/ TestBed', () => {
+      const fixture = TestBed.createComponent(TestOneComponent);
+      fixture.detectChanges();
+      expect(fixture.componentInstance).toBeDefined();
+    });
+  });
+
+  describe('w/o selector', () => {
+    beforeEach(() => MockBuilder(TestTwoComponent));
+
+    it('should render w/ MockRender', () => {
+      expect(
+        MockRender(TestTwoComponent).point.componentInstance,
+      ).toBeDefined();
+    });
+
+    it('should render w/ TestBed', () => {
+      const fixture = TestBed.createComponent(TestTwoComponent);
+      fixture.detectChanges();
+      expect(fixture.componentInstance).toBeDefined();
+    });
+  });
+});
---
+++ src/tests/issue-4282/global.spec.ts
@@ -0,0 +1,55 @@
+import {
+  Component,
+  inject,
+  Injectable,
+  NgModule,
+} from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+import { isMockOf, MockRender, ngMocks } from 'ng-mocks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TargetService {
+  name = 'real';
+}
+
+@Component({
+  selector: 'target',
+  template: `{{ service.name }}`,
+})
+export class TargetComponent {
+  readonly service = inject(TargetService);
+}
+
+@NgModule({
+  declarations: [TargetComponent],
+  exports: [TargetComponent],
+})
+class TargetModule {}
+
+ngMocks.globalMock(TargetService);
+ngMocks.defaultMock(TargetService, () => ({
+  name: 'mock',
+}));
+
+// @see https://github.com/help-me-mom/ng-mocks/issues/4282
+describe('issue-4282:global', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [TargetModule],
+    }).compileComponents(),
+  );
+
+  it('mocks the injected service', () => {
+    const fixture = MockRender(TargetComponent);
+    expect(ngMocks.formatText(fixture)).toEqual('mock');
+    expect(
+      isMockOf(
+        fixture.point.componentInstance.service,
+        TargetService,
+      ),
+    ).toEqual(true);
+  });
+});
