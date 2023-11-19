dcd99a3e11356a960921040df395df383a2df887
satanTime <m@sudo.eu>
2023-11-19 11:53:17 +0100

    feat(ng-mocks): 14.2.0 version

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import './polyfills';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { NgModuleRef } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
-import { MockInstance, MockService, ngMocks } from 'ng-mocks';
-import { DefaultTitleStrategy, TitleStrategy } from '@angular/router';
+
+import { MockInstance, ngMocks } from 'ng-mocks'; // eslint-disable-line import/order
 
 // In case, if you use @angular/router and Angular 14+.
 // You might want to set a mock of DefaultTitleStrategy as TitleStrategy.
 // A14 fix: making DefaultTitleStrategy to be a default mock for TitleStrategy
+import { DefaultTitleStrategy, TitleStrategy } from '@angular/router'; // eslint-disable-line import/order
+import { MockService } from 'ng-mocks'; // eslint-disable-line import/order
 ngMocks.defaultMock(TitleStrategy, () => MockService(DefaultTitleStrategy));
+
+// Usually, *ngIf and other declarations from CommonModule aren't expected to be mocked.
+// The code below keeps them.
+import { CommonModule } from '@angular/common'; // eslint-disable-line import/order
+import { ApplicationModule } from '@angular/core'; // eslint-disable-line import/order
+import { BrowserModule } from '@angular/platform-browser'; // eslint-disable-line import/order
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
 
 // TODO: remove when stackblitz has fixed it
 declare const jasmine: any;
